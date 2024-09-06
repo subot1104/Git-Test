5906646a34ea0a62ceeda983e3749a95c89865d4
Jose Hipolito <jhipolit@ut.utm.edu>
2024-09-06 15:52:10 -0500

    Added product function. Added documentation for sum and prod functions.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,29 @@
 
 using namespace std;
 
+
+/**
+ * Adds all positive integers from 1 to n"
+ *
+ * @param int n inputted integer
+ * @pre There is an integer to be inputted
+ * @return int the total sum from 1 to n
+ * @post 
+ * 
+ */
 int sum (int n);
+
+
+/**
+ * Multiplies all positive integers from 1 to n
+ *
+ * @param int n inputted integer
+ * @pre There is an integewr to be inputted
+ * @return int the total product from 1 to n
+ * @post 
+ * 
+ */
+int prod (int n);
 
 int main() {
   int n;
@@ -13,8 +35,10 @@ int main() {
   cout << "Please enter an integer." << endl;
   cin >> n;
   
-  cout << sum (n) << endl;
-  
+  cout << "The sum from 1 to " << n << " is " << sum (n) << '.' << endl;
+
+  cout << "The product from 1 to " << n << " is " << prod (n) << '.' << endl;
+
   return 0;
 }
 
@@ -26,3 +50,11 @@ int sum (int n) {
   
   return sum;
 }
+
+int prod (int n) {
+  int prod = 1;
+  for (int i = 1; i <= n; i++)
+    prod = prod * i;
+  return prod;
+}
+  
