3ecf03fd19925f3799dd2cda0ed2bb3c8c68eceb
Jose Hipolito <jhipolit@ut.utm.edu>
2024-09-06 12:13:46 -0500

    Added a funciton that sums 'n' numbers up to 'n.' Prompts to user to input an integer.

--- main.cpp
+++ main.cpp
@@ -3,8 +3,26 @@
 
 using namespace std;
 
+int sum (int n);
+
 int main() {
+  int n;
+  
   cout << "Hello World!" << endl;
   
+  cout << "Please enter an integer." << endl;
+  cin >> n;
+  
+  cout << sum (n) << endl;
+  
   return 0;
+}
+
+int sum (int n) {
+  int sum = 0;
+  
+  for (int i = 1; i <= n; i++)
+    sum += i;
+  
+  return sum;
 }
